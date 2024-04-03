b2fe983f694c1834aed192e3b3a02c5958e611ef
Yana0maybe0Shevchenko <142226235+Yana0maybe0Shevchenko@users.noreply.github.com>
2024-04-03 21:10:45 +0300

    Update task-3.js

--- js/task-3.js
+++ js/task-3.js
@@ -1,13 +1,35 @@
-function getElementWidth(content, padding, border) {
+class StringBuilder {
+    #value;
 
-    const contentWidth = parseFloat(content);
-    const paddingWidth = parseFloat(padding);
-    const borderWidth = parseFloat(border);
+    constructor(initialValue) {
+      this.#value = initialValue;
+    }
 
-    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
-    return totalWidth;
-}
+    getValue() {
+      return this.#value;
+    }
 
-console.log(getElementWidth("50px", "8px", "4px")); // 74
-console.log(getElementWidth("60px", "12px", "8.5px")); // 101
-console.log(getElementWidth("200px", "0px", "0px")); // 200
+    padEnd(str) {
+      this.#value += str;
+    }
+
+    padStart(str) {
+      this.#value = str + this.#value;
+    }
+
+    padBoth(str) {
+      this.#value = str + this.#value + str;
+    }
+  }
+  
+
+  const builder = new StringBuilder(".");
+  console.log(builder.getValue()); // "."
+  builder.padStart("^");
+  console.log(builder.getValue()); // "^."
+  builder.padEnd("^");
+  console.log(builder.getValue()); // "^.^"
+  builder.padBoth("=");
+  console.log(builder.getValue()); // "=^.^="
+  
+
